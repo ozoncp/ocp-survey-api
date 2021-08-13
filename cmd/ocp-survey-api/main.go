72e942509b994d11b2a18f0327f04c70276413ed
yuujiin <62110157+yuujiin@users.noreply.github.com>
2021-08-13 22:46:23 +0300

    Implement CRUD API stubs
    With gRPC to REST API gateway.

--- cmd/ocp-survey-api/main.go
+++ cmd/ocp-survey-api/main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"net"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+
+	"github.com/ozoncp/ocp-survey-api/internal/api"
+	desc "github.com/ozoncp/ocp-survey-api/pkg/ocp-survey-api"
+)
+
+const (
+	grpcPort     = ":8082"
+	httpPort     = ":8080"
+	grpcEndpoint = "localhost" + grpcPort
+)
+
+func run(ctx context.Context) error {
+	g, ctx := errgroup.WithContext(ctx)
+	g.Go(func() error { return runGRPC(ctx) })
+	g.Go(func() error { return runJSON(ctx) })
+
+	return g.Wait()
+}
+
+func runGRPC(_ context.Context) error {
+	listen, err := net.Listen("tcp", grpcPort)
+	if err != nil {
+		log.Fatal().Msgf("GRPC Listen: %v", err)
+		return err
+	}
+
+	s := grpc.NewServer()
+	desc.RegisterOcpSurveyApiServer(s, api.NewOcpSurveyApi())
+
+	err = s.Serve(listen)
+	if err != nil {
+		log.Fatal().Msgf("GRPC Serve: %v", err)
+	}
+	return err
+}
+
+func runJSON(ctx context.Context) error {
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+
+	err := desc.RegisterOcpSurveyApiHandlerFromEndpoint(ctx, mux, grpcEndpoint, opts)
+	if err != nil {
+		log.Fatal().Msgf("Register JSON API handler: %v", err)
+		return err
+	}
+
+	err = http.ListenAndServe(httpPort, mux)
+	if err != nil {
+		log.Fatal().Msgf("JSON Serve: %v", err)
+	}
+	return err
+}
 
 func main() {
-	fmt.Println("Ozon Code Platform Survey service")
+	log.Info().Msg("Ozon Code Platform Survey service started")
+
+	ctx := context.Background()
+
+	if err := run(ctx); err != nil {
+		log.Fatal().Msgf("Run service: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,18 @@ module github.com/ozoncp/ocp-survey-api
 go 1.16
 
 require (
+	github.com/envoyproxy/protoc-gen-validate v0.6.1
 	github.com/golang/mock v1.6.0
+	github.com/golang/protobuf v1.5.2
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.14.0
+	github.com/rs/zerolog v1.23.0
+	golang.org/x/net v0.0.0-20210805182204-aaa1db679c0d // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210811021853-ddbe55d93216
+	google.golang.org/grpc v1.40.0
+	google.golang.org/protobuf v1.27.1
 )
