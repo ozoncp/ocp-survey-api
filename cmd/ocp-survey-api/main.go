9767f4079566c6682cc6874490a14e1778a64074
yuujiin <62110157+yuujiin@users.noreply.github.com>
2021-08-16 15:36:25 +0300

    Add graceful shutdown

--- cmd/ocp-survey-api/main.go
+++ cmd/ocp-survey-api/main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/rs/zerolog/log"
@@ -20,6 +23,25 @@ const (
 	grpcEndpoint = "localhost" + grpcPort
 )
 
+// regSignalHandler отменяет контекст при получении сигналов SIGQUIT, SIGINT, SIGTERM.
+func regSignalHandler(ctx context.Context) context.Context {
+	ctx, cancel := context.WithCancel(ctx)
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		defer signal.Stop(stop)
+		<-stop
+		log.Info().Msg("Stop signal received")
+		cancel()
+	}()
+
+	return ctx
+}
+
+// run запускает gRPC-сервер и JSON-gateway к нему.
+// При отмене контекста ctx будет выполнен graceful stop.
 func run(ctx context.Context) error {
 	g, ctx := errgroup.WithContext(ctx)
 	g.Go(func() error { return runGRPC(ctx) })
@@ -28,21 +50,33 @@ func run(ctx context.Context) error {
 	return g.Wait()
 }
 
-func runGRPC(_ context.Context) error {
+func runGRPC(ctx context.Context) error {
 	listen, err := net.Listen("tcp", grpcPort)
 	if err != nil {
-		log.Error().Msgf("GRPC Listen: %v", err)
+		log.Error().Err(err).Msg("GRPC: Listen")
 		return err
 	}
 
 	srv := grpc.NewServer()
 	desc.RegisterOcpSurveyApiServer(srv, api.NewOcpSurveyApi())
 
-	err = srv.Serve(listen)
-	if err != nil {
-		log.Error().Msgf("GRPC Serve: %v", err)
+	srvErr := make(chan error)
+	go func() {
+		if err := srv.Serve(listen); err != nil {
+			srvErr <- err
+		}
+	}()
+
+	select {
+	case err := <-srvErr:
+		log.Error().Err(err).Msg("GRPC: Serve")
+		return err
+
+	case <-ctx.Done():
+		srv.GracefulStop()
 	}
-	return err
+
+	return nil
 }
 
 func runJSON(ctx context.Context) error {
@@ -51,24 +85,41 @@ func runJSON(ctx context.Context) error {
 
 	err := desc.RegisterOcpSurveyApiHandlerFromEndpoint(ctx, mux, grpcEndpoint, opts)
 	if err != nil {
-		log.Error().Msgf("Register JSON API handler: %v", err)
+		log.Error().Err(err).Msg("JSON: Register API handler")
 		return err
 	}
 
 	srv := &http.Server{Addr: httpPort, Handler: mux}
-	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-		log.Error().Msgf("JSON Serve: %v", err)
+
+	srvErr := make(chan error)
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			srvErr <- err
+		}
+	}()
+
+	select {
+	case err := <-srvErr:
+		log.Error().Err(err).Msg("JSON: Serve")
 		return err
+
+	case <-ctx.Done():
+		err := srv.Shutdown(context.Background())
+		if err != nil {
+			log.Error().Err(err).Msg("JSON: Shutdown")
+			return err
+		}
 	}
+
 	return nil
 }
 
 func main() {
 	log.Info().Msg("Ozon Code Platform Survey service started")
 
-	ctx := context.Background()
-
+	ctx := regSignalHandler(context.Background())
 	if err := run(ctx); err != nil {
-		log.Fatal().Msgf("Run service: %v", err)
+		log.Fatal().Err(err).Msg("Service stopped on error")
 	}
+	log.Info().Msg("Service exited")
 }
